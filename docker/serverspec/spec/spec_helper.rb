746a9ad05d9f974e356df152a2ea06c74c91196e
Atsushi Nagase <a@ngs.io>
2015-09-26 05:17:28 +0900

    LXC backend

--- docker/serverspec/spec/spec_helper.rb
+++ docker/serverspec/spec/spec_helper.rb
@@ -17,18 +17,10 @@ Excon.defaults[:ssl_verify_peer] = false
 if ENV['CIRCLECI']
   module Docker
     class Container
-      def exec(command, opts = {}, &block)
-        command[2] = command[2].inspect
-        cmd = %Q{sudo lxc-attach -n #{self.id} -- #{command.join(' ')}}
-        stdin, stdout, stderr, wait_thread = Open3.popen3 cmd
-        [stdout.read, [stderr.read], wait_thread.value.exitstatus]
-      end
-
       def remove(options={})
         # do not delete container
       end
       alias_method :delete, :remove
-      alias_method :kill, :remove
     end
   end
 end
